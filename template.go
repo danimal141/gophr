f8549a0963b8fdb9a5b89a0da93fcaa767cc95eb
danimal141 <hideaki.ishii1204@gmail.com>
2016-10-23 10:43:33 +0900

    Implement layout.tmpl

--- template.go
+++ template.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
 	"net/http"
 )
 
+var layoutFuncs = template.FuncMap{
+	"yield": func() (string, error) {
+		return "", fmt.Errorf("yield called inappropriately")
+	},
+}
+var layout = template.Must(
+	template.New("layout").Funcs(layoutFuncs).ParseFiles("templates/layout.tmpl"),
+)
+
 var templates = template.Must(template.New("t").ParseGlob("templates/**/*.tmpl"))
 
 func RenderTemplate(w http.ResponseWriter, r *http.Request, name string, data interface{}) {
-	err := templates.ExecuteTemplate(w, name, data)
+	funcs := template.FuncMap{
+		"yield": func() (template.HTML, error) {
+			buf := bytes.NewBuffer(nil)
+			err := templates.ExecuteTemplate(buf, name, data) // Write to buffer instead of ResponseWriter
+			return template.HTML(buf.String()), err
+		},
+	}
+	layoutClone, err := layout.Clone()
+	if err != nil {
+		fmt.Sprintf("Error: ", err)
+	}
+	layoutClone.Funcs(funcs)
+
+	err = layoutClone.Execute(w, data)
 	if err != nil {
 		http.Error(
 			w,
-			fmt.Sprintf("Error:", err),
+			fmt.Sprintf("Error: ", err),
 			http.StatusInternalServerError,
 		)
 	}
