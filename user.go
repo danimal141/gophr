abda420cc743ea5c1e0fb292e75f1f8ac09ecc1b
danimal141 <hideaki.ishii1204@gmail.com>
2016-10-29 17:39:02 +0900

    Implement a function to create a new user

--- user.go
+++ user.go
@@ -1,8 +1,37 @@
 package main
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
 	ID             string
 	Username       string
 	Email          string
 	HashedPassword string
+}
+
+const (
+	passwordMinLength = 8
+	userIDLength      = 16
+)
+
+func NewUser(username, email, password string) (User, error) {
+	user := User{Username: username, Email: email}
+
+	if username == "" {
+		return user, errNoUsername
+	}
+	if email == "" {
+		return user, errNoEmail
+	}
+	if password == "" {
+		return user, errNoPassword
+	}
+	if len(password) < passwordMinLength {
+		return user, errPasswordTooShort
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	user.HashedPassword = string(hashedPassword)
+	user.ID = GenerateID("usr", userIDLength)
+	return user, err
 }
